3a8cd8dd15fd98aab215f0d868ea46660fb81ffe
Zinthrow <33006657+Zinthrow@users.noreply.github.com>
2017-10-23 15:38:29 -0500

    Update Euler.py

--- Euler.py
+++ Euler.py
@@ -1,5 +1,7 @@
 import os
 
+import os
+
 #class vertice(object):
 #    def __init__(self)
 
@@ -8,12 +10,17 @@ if os.path.exists(filename):
     kmers = open(filename, 'r')
 
 
-lizt = []
-test = {}
+
+graph = {} #the key = Nucleotide seq and the value = [indegrees],[outdegrees]
+
 for kmer in kmers:
+    prev = None
     for x,y in zip(kmer[0:], kmer[1:]):
         if y != "\n":
-            lizt.append(x+y)
-            test[x+y] =
-    
-print (lizt)
+            if x+y not in graph:
+                graph[x+y] = [[],[]]
+            if prev != None:
+                graph[prev][1].append(x+y)#makes outdegrees for prev                
+                graph[x+y][0].append(prev)#sets indegrees for x+y
+            prev = x+y
+            
