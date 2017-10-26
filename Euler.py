9a9b54be8c39e76356c8b2f655b320cfc4969d3c
Zinthrow <33006657+Zinthrow@users.noreply.github.com>
2017-10-26 12:10:06 -0500

    Update Euler.py

--- Euler.py
+++ Euler.py
@@ -23,4 +23,23 @@ for kmer in kmers:
                 graph[prev][1].append(x+y)#makes outdegrees for prev                
                 graph[x+y][0].append(prev)#sets indegrees for x+y
             prev = x+y
-            
+keys = sorted(keys)            
+for edges in keys:
+    for degrees in edges:
+        degrees = sorted(degrees)
+fake_start = []
+for start in keys:
+    while start not in fake_start:
+        path = str(start)
+        copygraph = graph
+        opennode ={}
+        if copygraph[start][0] is not []:
+            opennode[start] = True
+        while openode != {}:
+            dupligraph = copygraph
+            subpath = start
+            x = start
+            while copygraph[x][1] != []:
+                x = dupligraph[x][1][0]
+                subpath = subpath + x
+                dupligraph[x][1].pop(x)
