a8bf1c483f5b71ff193dee56a4e8e7de6963a775
Zinthrow <33006657+Zinthrow@users.noreply.github.com>
2017-10-26 23:59:00 -0500

    Update Euler.py

--- Euler.py
+++ Euler.py
@@ -29,17 +29,32 @@ for edges in keys:
         degrees = sorted(degrees)
 fake_start = []
 for start in keys:
-    while start not in fake_start:
-        path = str(start)
+    path = str(start)
+    copygraph = graph
+    opennode ={}
+    if copygraph[start][0] is not []:
+        opennode[start] = True
+    while openode != {}:
         copygraph = graph
-        opennode ={}
-        if copygraph[start][0] is not []:
-            opennode[start] = True
-        while openode != {}:
-            dupligraph = copygraph
-            subpath = start
-            x = start
-            while copygraph[x][1] != []:
-                x = dupligraph[x][1][0]
-                subpath = subpath + x
-                dupligraph[x][1].pop(x)
+        x = start
+        subpath = x
+        for smer in opennode:#moves used degrees to end of degrees list
+            if len(smer) == 4:
+                copygraph[smer[0:2]][1].pop(smer[2:]) #moves outdegrees
+                copygraph[smer[0:2]][1].append(smer[2:])
+                copygraph[smer[2:]][0].pop(smer[0:2]) #moves indegrees
+                copygraph[smer[2:]][0].append(smer[0:2])
+        while copygraph[x][1] != []:
+            x = copygraph[x][1]
+            subpath = subpath + x[1]
+            copygraph[x][1].pop(0)
+            #if copygraph[x][1][0] in
+            if copygraph[x][1] == []:
+                if isinstance(x+copygraph[x][1][0],opennode):
+                    opennode.pop(x+copygraph[x][1][0])
+            elif copygraph[x][1] != []:
+                opennode[x+copygraph[x][1][0]] = True
+            copygraph[x][2] = len(copygraph[x][1])-len(copygraph[x][0])
+            if isinstance(start,opennode):
+                opennode.pop(start)
+            #was figuring a way to tie balance into everything
